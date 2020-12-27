b472e82a73f20e90fa93700d685592cb666e3703
Aldo Rodríguez <ae.rodriguez.ext@orangebank.es>
2020-12-27 20:12:55 +0100

    Implement servlet context listener to initialize course data base

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Improve CourseApp version by adding CI integration
 
+```shell
+docker image build -t tomcat-course-app -f docker/Dockerfile .
+docker container run -it --name course-app --publish 8081:8080 tomcat-course-app
+```
+
--- src/main/java/com/codetodo/courseapp/dao/H2ConnectionFactory.java
+++ src/main/java/com/codetodo/courseapp/dao/H2ConnectionFactory.java
@@ -6,28 +6,28 @@ import java.sql.SQLException;
 
 public class H2ConnectionFactory implements ConnectionFactory {
 
-	private String url = "jdbc:h2:~/test";
-	private String user = "sa";
-	private String pass = "";
+    private String url = "jdbc:h2:~/test";
+    private String user = "sa";
+    private String pass = "";
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public void setUser(String user) {
-		this.user = user;
-	}
+    public void setUser(String user) {
+        this.user = user;
+    }
 
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 
-	public Connection getConnection() {
-		try {
-			org.h2.Driver.load();
-			return DriverManager.getConnection(url, user, pass);
-		} catch (SQLException ex) {
-			throw new DataAccessException("An error ocurred ");
-		}
-	}
+    public Connection getConnection() {
+        try {
+            org.h2.Driver.load();
+            return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException ex) {
+            throw new DataAccessException("An error occurred ");
+        }
+    }
 }
